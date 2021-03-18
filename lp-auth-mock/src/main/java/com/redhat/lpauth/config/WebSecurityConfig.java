1eb483e37f947fc1434d7e95dacf5fe00276316b
dumdumgw@gmail.com <Shrmsdnr1217!>
2021-03-18 22:33:20 +0900

    add lp-mock

--- lp-auth-mock/src/main/java/com/redhat/lpauth/config/WebSecurityConfig.java
+++ lp-auth-mock/src/main/java/com/redhat/lpauth/config/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/public/login").permitAll()
+                .antMatchers("/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
